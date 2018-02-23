610660b66ffbc05613b6b9ce974918c339e2e4e7
Faith Chau <faith_chau@Faiths-MacBook-Pro.local>
2018-02-23 12:27:00 -0800

    mongodb connected

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
   googleClientID:
     '946005562762-qls52ulkrmharbd9kfjvf52rq28eapgq.apps.googleusercontent.com',
-  googleClientSecret: 'rUWKa3w2RLaaAT3vy8aFSMr_'
+  googleClientSecret: 'rUWKa3w2RLaaAT3vy8aFSMr_',
+    mongoUrl: 'mongodb://cloudlogger:Nimbu%242018@ds147518.mlab.com:47518/cloudlogger'
 };
--- server/index.js
+++ server/index.js
@@ -2,32 +2,38 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+const mongoose = require ('mongoose');
 
 const app = express();
 
+//connect to mLab with mongoose
+mongoose.connect(keys.mongoUrl);
+
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mLab connection error'));
+
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile: ', profile);
-    }
-  )
+    new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken, refreshToken, profile, done) =>{
+        console.log('access token', accessToken);
+        console.log('refresh token', refreshToken);
+        console.log('profile: ', profile);
+        }
+    )
 );
 
 app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
 );
 
 app.get('/auth/google/callback', passport.authenticate('google'));
-
 const PORT = process.env.PORT || 5000;
 app.listen(5000);
