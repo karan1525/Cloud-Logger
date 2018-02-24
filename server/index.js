4f975f3d7243553b7e476f20e258a016f921eecf
Conover Wang <kilaplaya@gmail.com>
2018-02-23 21:47:01 -0800

    user document save + added mongoose to package.json

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
 const mongoose = require ('mongoose');
-
+const User = require('./models/User.js');
 const app = express();
 
 //connect to mLab with mongoose
@@ -20,9 +20,22 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) =>{
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile: ', profile);
+        // console.log('access token', accessToken);
+        // console.log('refresh token', refreshToken);
+        // console.log('profile: ', profile);
+        // console.log('------------------')
+        User.count({userId: profile.id}, function (err, count){
+          if(count==0){
+            var instance = new User();
+            instance.userId = profile.id;
+            instance.name = profile.displayName;
+            instance.email = profile.emails[0].value;
+            instance.imageUrl = profile.photos[0].value;
+            instance.save(function (err) {
+              console.log(err);
+            });
+          }
+        });
         }
     )
 );
