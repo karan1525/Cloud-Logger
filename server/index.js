02383a15ef2599cfbb847c98c3151fa8dece8a9c
Faith Chau <faith_chau@Faiths-MacBook-Pro.local>
2018-03-08 00:06:26 -0800

    file route added

--- server/index.js
+++ server/index.js
@@ -3,12 +3,15 @@ const keys = require('./config/keys');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require ('body-parser')
 require('./models/Log');
 require('./models/User');
 require('./services/passport');
 
 const fs = require('fs');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(
   cookieSession({
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -3,9 +3,21 @@ const fs = require('fs');
 const fileFunctions = require('../file/fileFunctions.js');
 module.exports = app => {
   app.post('/test', function(req, res) {
-    fs.readFile('/Users/cdub/Desktop/test.txt', function(err, data) {
-      fileFunctions.file_upload('dummyUserId', data);
-    });
+   fs.readFile('/Users/cdub/Desktop/test.txt', function(err, data) {s
+
+       fileFunctions.file_upload( 'dummyName',data);
+
+   });
+
+
+
     res.status(200).send('file was uploaded');
+  });
+
+
+  app.put('/test', function(req, res){
+    console.log(req.body.message);
+    res.status(200).send(req.body);
+
   });
 };
