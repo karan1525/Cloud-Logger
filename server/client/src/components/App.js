28f8e6831e85cb95e5f562b90f13eaf27eff4f67
Karan Bhargava <karan1525@yahoo.co.in>
2018-03-10 18:35:21 -0800

    Added dummy route and made reusable components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,11 +4,19 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
 import Landing from './Landing';
 
+const fileUpload = () => <h2>File Upload!</h2>;
+
 class App extends Component {
   render() {
     return (
       <div>
-        <Landing />
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/upload" component={fileUpload} />
+          </div>
+        </BrowserRouter>
       </div>
     );
   }
--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import Header from './Header';
 import '../styling/Body.css';
 
 class Landing extends Component {
   render() {
     return (
       <div>
-        <Header />,
         <div className="Body">
           <h1>LOG YOUR FILES.</h1>
           <h1>GAIN INSIGHT FROM ERRORS.</h1>
