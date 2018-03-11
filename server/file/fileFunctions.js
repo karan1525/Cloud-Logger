338f4d4ed74cf246c2f6ffb493ab947a854c7309
Faith Chau <faith_chau@Faiths-MacBook-Pro.local>
2018-03-11 16:10:18 -0700

    file rename, overwrite, delete routes implemented; duplicate file name check implemented

--- server/file/fileFunctions.js
+++ server/file/fileFunctions.js
@@ -1,24 +1,74 @@
-const mongoose = require('mongoose');
-const Log = mongoose.model('Log');
-const fs = require('fs');
+const mongoose = require("mongoose");
+const Log = mongoose.model("Log");
+const fs = require("fs");
 
-function file_upload(id, file) {
+function file_upload(id, fileName, file) {
   const log = new Log({
     userId: id,
-    logFile: file,
+    logFileName: fileName,
+    logFile: file
   });
   log.save();
 }
 
-function file_delete(fileId){
-  Log.findByIdAndRemove(fileId, (err, file)=>{
-    if (err) throw err;
+function file_delete(userId, fileName) {
+  Log.findOneAndRemove(
+    { userId: userId, logFileName: fileName },
+    (err, file) => {
+      if (err) throw err;
 
-    console.log("file removed");
+      if (file) 
+        console.log("file removed");
+      else 
+        console.log("file not found");
+      //todo: how to let frontend know theres a problem?
+    }
+  );
+}
 
-    });
+function file_overwrite(userId, fileName, file) {
+  Log.findOneAndUpdate(
+    { userId: userId, logFileName: fileName },
+    { logFile: file },
+    (err, file) => {
+      if (err) throw err;
+
+      if (file) 
+        console.log("file overwrote");
+      else 
+        console.log("file not found");
+    }
+  );
+}
+
+function file_find(userId, fileName, callback) {
+  Log.findOne({ userId: userId, logFileName: fileName }, (err, file) => {
+    if (err) callback(err,false);
+
+    if (file) 
+      callback(err, true);
+    else 
+      callback(err, false);
+  });
+}
+
+function file_rename(userId, oldFileName, newFileName){
+  Log.findOneAndUpdate({userId: userId, logFileName: oldFileName},
+  {logFileName: newFileName},
+  (err, entry)=>{
+    if(err) throw err;
+
+    if(entry)
+      console.log("file renamed")
+    else
+      console.log("file not found")
+  });
 }
 
 module.exports = {
-  file_upload, file_delete
+  file_upload,
+  file_delete,
+  file_overwrite,
+  file_find,
+  file_rename
 };
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -1,35 +1,79 @@
-const fs = require('fs');
-const fileFunctions = require('../file/fileFunctions.js');
-const formidable = require('formidable');
+const fs = require("fs");
+const fileFunctions = require("../file/fileFunctions.js");
+const formidable = require("formidable");
 
 module.exports = app => {
-  app.post('/upload', function(req, res) {
+  //upload new file 
+  app.post("/upload", function(req, res) {
     const form = new formidable.IncomingForm();
 
     form.parse(req, function(err, fields, files) {
       if (err) {
         console.log(err.stack);
-        res.status(500).send('parsing failed');
+        res.status(500).send("parsing failed");
       }
 
-      fs.readFile(files.fileUploaded.path, function(err, data) {
-        if (err) {
-          console.log(err.stack);
-          res.status(500).send('upload failed');
-        }
+      fileFunctions.file_find(fields.userId, files.fileUploaded.name, (err, found)=>{
+        //Todo: need to redirect when file is found ; currently throwning an err
+        if(err) res.status(500).send('file_find error happened')
 
-        fileFunctions.file_upload(fields.userId, data);
+        if (found){
+          res.status(500).send("fileName already exist");
+        }
+        else{  
+          fs.readFile(files.fileUploaded.path, function(err, data) {
+            if (err) {
+              console.log(err.stack);
+              return res.status(500).send("upload failed");
+            }
+
+            fileFunctions.file_upload(
+              fields.userId,
+              files.fileUploaded.name,
+              data
+            );
+          });
+          res.status(200).send("file was uploaded");
+        }
+      });
+      
+    });
+  });
+
+//rename file
+  app.put('/upload/rename/:userId/:oldFileName/:newFileName', function(req, res){
+    fileFunctions.file_rename(req.params.userId, req.params.oldFileName, req.params.newFileName);
+
+    res.status(200).send("file renamed");
+
+  });
+
+//overwrite existing file
+  app.put("/upload/overwrite", function(req, res) {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      if (err) throw err;
+
+      fs.readFile(files.fileUploaded.path, function(err, data) {
+        if (err) throw err;
+
+        fileFunctions.file_overwrite(
+          fields.userId,
+          files.fileUploaded.name,
+          data
+        );
       });
     });
 
-    res.status(200).send('file was uploaded');
+    res.status(200).send("file was overwritten");
   });
 
-  app.delete('/delete/file/:logId', function(req,res){
-      //if(err)res.status(500).send('file delete unsucessful')
+//delete existing file
+  app.delete("/delete/file/:userId/:fileName", function(req, res) {
 
-      fileFunctions.file_delete(req.params.logId);
+    fileFunctions.file_delete(req.params.userId, req.params.fileName);
 
-      res.status(200).send('file delete successful')
+    res.status(200).send("file delete successful");
   });
 };
