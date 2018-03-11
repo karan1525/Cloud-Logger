f9ba0562e7892b98ff228a7ffa5bc9d2f6706036
Karan Bhargava <karan1525@yahoo.co.in>
2018-03-10 18:23:35 -0800

    Added reduxLogger

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,12 +3,13 @@ import 'materialize-css/dist/js/materialize.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './components/App';
 
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(() => [], {}, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
