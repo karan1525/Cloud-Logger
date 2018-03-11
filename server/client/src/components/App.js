60f04b6643b482a6e1a8ef84b231dbac086bfdcd
Karan Bhargava <karan1525@yahoo.co.in>
2018-03-10 18:30:50 -0800

    Added initial router setup

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import Header from './Header';
 import Landing from './Landing';
 
 class App extends Component {
