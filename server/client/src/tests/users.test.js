d6ac98ce04ce8878a246470d97324baf8f4c5f8a
Cynthia Li <xinyan.li1118@gmail.com>
2018-03-14 18:42:00 -0700

    add unit test for reducers

---
+++ server/client/src/tests/users.test.js
@@ -0,0 +1,11 @@
+// unit test for the authReducer
+import user from '../reducers/authReducer';
+import { FETCH_USER } from '../actions/types';
+
+describe('Login reducer', ()=> {
+  it('shoud handle FETCH_USER', ()=> {
+    expect(user(undefined,{
+      type: FETCH_USER
+    })).toEqual(false);
+ });
+});
