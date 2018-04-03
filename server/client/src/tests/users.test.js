2b763fe3868df4655398b085a2b687e7aff45daf
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-03 09:15:53 -0700

    Ran linter

--- server/client/src/tests/users.test.js
+++ server/client/src/tests/users.test.js
@@ -2,10 +2,12 @@
 import user from '../reducers/authReducer';
 import { FETCH_USER } from '../actions/types';
 
-describe('Login reducer', ()=> {
-  it('shoud handle FETCH_USER', ()=> {
-    expect(user(undefined,{
-      type: FETCH_USER
-    })).toEqual(false);
- });
+describe('Login reducer', () => {
+  it('shoud handle FETCH_USER', () => {
+    expect(
+      user(undefined, {
+        type: FETCH_USER
+      })
+    ).toEqual(false);
+  });
 });
