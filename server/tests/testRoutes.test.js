56009d04e794f3ac3ae5198f40573b443092b768
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-03 11:23:52 -0700

    Changed test.js descriptions

--- server/tests/testRoutes.test.js
+++ server/tests/testRoutes.test.js
@@ -1,22 +1,24 @@
 const request = require('supertest');
-const app = require('../index.js')
+const app = require('../index.js');
 
-
-describe('Test the test path', () => {
-    test('It should response the GET method', (done) => {
-        request(app).get('/status200ok').then((response) => {
-            expect(response.statusCode).toBe(200);
-            done();
-        });
-    });
+describe('Test the test path with GET', () => {
+  test('It should response the GET method', done => {
+    request(app)
+      .get('/status200ok')
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
 });
 
-
-describe('Test the test path', () => {
-    test('It should response the POST method with 404 not found', (done) => {
-        request(app).post('/status200ok').then((response) => {
-            expect(response.statusCode).toBe(404);
-            done();
-        });
-    });
+describe('Test the test path with POST', () => {
+  test('It should response the POST method with 404 not found', done => {
+    request(app)
+      .post('/status200ok')
+      .then(response => {
+        expect(response.statusCode).toBe(404);
+        done();
+      });
+  });
 });
