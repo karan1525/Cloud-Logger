4dc1d0f2eb7d9b6cafa1db3d58925f66891b8fa9
jenearley <jen0earley@gmail.com>
2018-04-05 11:14:02 -0700

    Added file upload form for files and checking file ext for files.

--- server/client/src/components/Upload.js
+++ server/client/src/components/Upload.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 
-var _validFileExts = [".txt", ".csv"];
 
 class Upload extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {file:null};
+        
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+   }
+    
+   handleChange(event) {
+       this.setState({file: event.target.files[0]});
+   }
+    
+   handleSubmit(event) {
+       event.preventDefault;
+       var filename = this.state.file.name;
+       var ext = filename.split('.').pop();
+       if(ext != 'txt') {
+          alert('Sorry, ' + ext + ' files are not accepted. Accepted files are txt only.');
+       } else {
+          alert('A file was submitted: ' + filename + 'With ext: ' + ext);
+       }
+   }
+    
   render() {
     return (
       <div>
         <h1>This is the upload page.</h1>
-        <form >
+        <form onSubmit={this.handleSubmit}>
           <label>
-            <input type="file" />
+             <input type="file" file={this.state.file} onChange={this.handleChange}/>
           </label>
-          <input type="submit" value="Upload" />
+             <input type="submit" value="Upload" />
         </form>
       </div>
     );
