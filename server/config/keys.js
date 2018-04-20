2218eafcda524865a580bbc241a5ae54b94a06f9
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-20 01:08:05 -0700

    Pushing prod keys. Deleting soon

--- server/config/keys.js
+++ server/config/keys.js
@@ -6,3 +6,10 @@ module.exports = {
     'mongodb://cloudlogger:Nimbu%242018@ds147518.mlab.com:47518/cloudlogger',
   cookieKey: 'uiaodjasldksjdwiodjlksa923021woas'
 };
+
+//DEV KEYS
+
+//GOOGLE CLIENT -> 67672504776-jjug6ltqvqjef0pdcrbvor3cm7ehvk9j.apps.googleusercontent.com
+//GOOGLE SECRET -> vKzHUPXY7mZW_LBHEVSHNZlm
+//MONGO URL -> mongodb://spiderman:AuNtMaY@ds139277.mlab.com:39277/cloudlogger-prod
+//COOKIE KEY -> asjdkashd12y98equiwodlau120qdaslkd
