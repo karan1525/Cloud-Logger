015a6878c427f6f10c11e6a5af44f645dc3989dc
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-20 02:10:01 -0700

    Removed all prod keys from plain text

--- server/config/keys.js
+++ server/config/keys.js
@@ -6,8 +6,3 @@ if (process.env.NODE_ENV === 'production') {
   //RETURN DEV KEYS
   module.exports = require('./dev');
 }
-
-//GOOGLE CLIENT -> 67672504776-jjug6ltqvqjef0pdcrbvor3cm7ehvk9j.apps.googleusercontent.com
-//GOOGLE SECRET -> vKzHUPXY7mZW_LBHEVSHNZlm
-//MONGO URL -> mongodb://spiderman:AuNtMaY@ds139277.mlab.com:39277/cloudlogger-prod
-//COOKIE KEY -> asjdkashd12y98equiwodlau120qdaslkd
