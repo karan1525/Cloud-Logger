f5cd8b34a83a08a2d9aede9f165630bbd93c6cc3
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-20 02:07:41 -0700

    Divided keys into prod and dev

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,13 +1,11 @@
-module.exports = {
-  googleClientID:
-    '946005562762-qls52ulkrmharbd9kfjvf52rq28eapgq.apps.googleusercontent.com',
-  googleClientSecret: 'rUWKa3w2RLaaAT3vy8aFSMr_',
-  mongoUrl:
-    'mongodb://cloudlogger:Nimbu%242018@ds147518.mlab.com:47518/cloudlogger',
-  cookieKey: 'uiaodjasldksjdwiodjlksa923021woas'
-};
-
-//DEV KEYS
+// FIGURE OUT WHICH KEYS TO RETURN
+if (process.env.NODE_ENV === 'production') {
+  //RETURN PROD KEYS
+  module.exports = require('./prod');
+} else {
+  //RETURN DEV KEYS
+  module.exports = require('./dev');
+}
 
 //GOOGLE CLIENT -> 67672504776-jjug6ltqvqjef0pdcrbvor3cm7ehvk9j.apps.googleusercontent.com
 //GOOGLE SECRET -> vKzHUPXY7mZW_LBHEVSHNZlm
