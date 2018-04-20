6da3d64f367b19b8ab6616cd066d3171548b9175
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-20 01:49:04 -0700

    Added which html file to serve when on heroku

--- server/index.js
+++ server/index.js
@@ -30,6 +30,20 @@ mongoose.connect(keys.mongoUrl);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mLab connection error'));
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+
+  app.use(express.static('server/client/build')); //based on project structure
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
