dc9078f8e3d81ed01067bb97e0f99e86b7d8b1cd
Karan Bhargava <karan1525@yahoo.co.in>
2018-04-26 10:32:53 -0700

    Initial styling for error page

--- server/client/src/components/files/ErrorAnalysis.js
+++ server/client/src/components/files/ErrorAnalysis.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 class ErrorAnalysis extends Component {
+  componentDidMount() {
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false,
+      onClose: function() {
+        $(document.activeElement).blur();
+      }
+    });
+  }
+
   render() {
-    return <h1>{this.props.location.state.fileName}</h1>;
+    return (
+      <div>
+        <h1>{this.props.location.state.fileName}</h1>
+        <div class="row">
+          <div class="input-field col s6">
+            <input
+              placeholder="YYYY/MM/DD"
+              id="start_date"
+              type="date"
+              className="datepicker"
+            />
+            <label style={{ fontSize: '25px' }} for="start_date">
+              Start Date
+            </label>
+          </div>
+          <div class="input-field col s6">
+            <input
+              placeholder="YYYY/MM/DD"
+              id="end_date"
+              type="date"
+              className="datepicker"
+            />
+            <label style={{ fontSize: '25px' }} for="end_date">
+              End Date
+            </label>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
