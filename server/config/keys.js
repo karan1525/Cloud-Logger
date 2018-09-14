3941e8f4922e06e64eba27439dd36dd44351fa44
Karan Bhargava <karan1525@yahoo.co.in>
2018-09-13 23:43:37 -0700

    Added back keys.js

---
+++ server/config/keys.js
@@ -0,0 +1,8 @@
+// FIGURE OUT WHICH KEYS TO RETURN
+if (process.env.NODE_ENV === "production") {
+  //RETURN PROD KEYS
+  module.exports = require("./prod");
+} else {
+  //RETURN DEV KEYS
+  module.exports = require("./dev");
+}
