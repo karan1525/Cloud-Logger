30714f0913f32c473be267b438fe7d2e9256937e
Karan Bhargava <karan1525@yahoo.co.in>
2018-09-13 23:42:34 -0700

    Deleted keys.js from Github.com

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-// FIGURE OUT WHICH KEYS TO RETURN
-if (process.env.NODE_ENV === 'production') {
-  //RETURN PROD KEYS
-  module.exports = require('./prod');
-} else {
-  //RETURN DEV KEYS
-  module.exports = require('./dev');
-}
